364061dee327d75b87c6371f19c58eace97245bd
kevin2-cyber <keduful@st.ug.edu.gh>
2022-05-17 01:14:01 +0000

    new

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:camera/camera.dart';
 import 'take_picture_screen.dart';
 import 'package:flutter/material.dart';
 
-
 Future<void> main() async {
   // Ensure that plugin services are initialized so that `availableCameras()`
   // can be called before `runApp()`
@@ -13,27 +12,15 @@ Future<void> main() async {
   // Obtain a list of the available cameras on the device.
   final cameras = await availableCameras();
 
-
-
-  runApp(CameraApp());
-}
-
-class CameraApp extends StatelessWidget {
-  CameraApp({Key? key}) : super(key: key);
-
-
   // Get a specific camera from the list of available cameras.
-  final firstCamera =  cameras.first;
+  final firstCamera = cameras.first;
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       theme: ThemeData.dark(),
       home: TakePictureScreen(
         camera: firstCamera,
       ),
-    );
-  }
+    ),
+  );
 }
-
-
