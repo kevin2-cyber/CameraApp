7558a5ccc49f3f0b6c011f3c140b33809ef8a2d4
kevin2-cyber <keduful@st.ug.edu.gh>
2022-05-17 01:00:35 +0000

    added other screens

---
+++ lib/display_picture_screen.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+// A widget that displays the picture taken by the user.
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({Key? key, required this.imagePath})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Display the Picture')),
+      // The image is stored as a file on the device. Use the `Image.file`
+      // constructor with the given path to display the image.
+      body: Image.file(File(imagePath)),
+    );
+  }
+}
